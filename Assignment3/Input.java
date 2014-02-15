78807ee5a6793a68cd0e9f69674d1c7e98c4efc0
natasftw <natasftw@hotmail.com>
2014-02-15 11:43:23 -0600

    Added Assignment3 skeleton

---
+++ Assignment3/Input.java
@@ -0,0 +1,23 @@
+package Assignment3;
+
+public class Input {
+
+/**
+ * The list of attributes possible for every input.
+ */
+	private String operation;
+	private String category;
+	private String name;
+	private double price;
+	private int quantity;
+	private int weight;
+	private boolean validInput;
+	private boolean isFragile;
+	private boolean isPerishable;
+	private String deliveryState;
+	
+	//create a constructor that uses the transaction string to create an input
+	
+	//create getters and setters
+	
+}
