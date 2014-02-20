89496646bfb3c7cd3a76c5c888bd5794de491687
natasftw <natasftw@hotmail.com>
2014-02-20 13:16:33 -0600

    Psuedocode added to client
    Psuedocode added to detail steps in each of the 5 operations.  Need to
    make real code and test it.  Mostly finished.

--- Assignment3/A3Driver.java
+++ Assignment3/A3Driver.java
@@ -13,24 +13,12 @@ import java.io.IOException;
 import java.util.*;
 
 public class A3Driver 
-	{
+{
 	  	  	  
 	  public static void main(String[] args) 
 	  {
-
-		Item testi = new Item("guitar", 2, 238.12, 10.12325);
-		Clothing testc = new Clothing("shirt", 1, 23.12, 1.2);
-		Electronics teste = new Electronics("laptop", 1, 1000, 15.3, "HI", true);
-		Grocery testg = new Grocery("apple", 1, 1, 1, true);
-		testi.printItemAttributes();
-		testi = testc;
-		testi.printItemAttributes();
-		testi = teste;
-		testi.printItemAttributes();
-		testi = testg;
-		testi.printItemAttributes();
-
-		
+		ArrayList<Item> shoppingCart = new ArrayList<Item>();
+		  
 		if (args.length != 1) 
 		{
 			System.err.println ("Error: Incorrect number of command line arguments");
@@ -48,7 +36,22 @@ public class A3Driver
 				boolean test = currentInput.getValidInput();
 				if(test)
 				{
-					System.out.println("Valid input object created.");
+					String operation = currentInput.getOperation();
+					if(operation.contentEquals("insert"))
+					{
+						processInsert(shoppingCart, currentInput);
+						int length = shoppingCart.size();
+						System.out.println("The cart has " + length + " items in it.");
+					} else if(operation.contentEquals("search")) {
+						boolean printResults = true;
+						processSearch(shoppingCart, currentInput, printResults);
+					} else if(operation.contentEquals("update")) {
+						processUpdate(shoppingCart, currentInput);
+					} else if(operation.contentEquals("delete")) {
+						processDelete(shoppingCart, currentInput);
+					} else {
+						processPrint(shoppingCart);
+					}
 				}
 			}
 		} 
@@ -62,17 +65,81 @@ public class A3Driver
 			System.err.println ("Error: IO exception. Exiting...");
 			e.printStackTrace();
 			System.exit(-1);
-		}
-		
-		//Open file; file name specified in args (command line)
-		
-		//Parse input, take appropriate actions.
-	
-		  
-		  
-		  
-		  
-		  
+		}	
+	  }
+
+/**
+ * A method to handle insertion of new items into cart
+ * @param cart - the cart's ArrayList
+ * @param inputData - the Input object containing the new item's details
+ */
+	  private static void processInsert(ArrayList<Item> cart, Input inputData)
+	  {
+		  //search cart for item
+		  //if item exists, add quantity to current item
+		  //otherwise, create an object and add it to cart
+		  System.out.println("Insert method called.");
+		  Clothing shirt = new Clothing("shirt", 1, 1, 1);
+		  cart.add(shirt);
+	  }
+	 
+/**
+ * A method to search through the ArrayList to find the desired item.
+ * @param cart - the cart's ArrayList
+ * @param inputData - the Input object with the item to search for
+ * @param showResults - surpresses results for use within other methods
+ */
+	  private static void processSearch(ArrayList<Item> cart, Input inputData, 
+			  boolean showResults)
+	  {
+		  //search the cart for the item
+		  //currently have a boolean, maybe replace with an int return
+		  //return lets other methods use this to check.  
+		  //show results lets this method print results to screen if needed
+		  System.out.println("Search method called.");
+	  }
+	  
+/**
+ * A method to update cart items to the new value.
+ * @param cart - the cart's ArrayList
+ * @param inputData - the Input object with the update information
+ */
+	  private static void processUpdate(ArrayList<Item> cart, Input inputData)
+	  {
+		  //search for item
+		  //if item exists, update it
+		  //else, send error message
+		  System.out.println("Update method called.");
+	  }
+	  
+/**
+ * A method to remove objects from the cart
+ * @param cart - the cart's ArrayList
+ * @param inputData - details about the object to remove
+ */
+	  private static void processDelete(ArrayList<Item> cart, Input inputData)
+	  {
+		  //look for all items with that name
+		  //delete all of them
+		  //update console with each deletion
+		  //list error message if none deleted
+		  System.out.println("Delete method called.");
+	  }
+	  
+/**
+ * A method to print all of the items in the cart
+ * @param cart - the cart itself
+ */
+	  private static void processPrint(ArrayList<Item> cart)
+	  {
+		  //check if cart is empty
+		  //if empty, say so
+		  //else, iterate through the cart calling showItemAttributes
+		  System.out.println("Print method called.");
+	  }
+}
+
+
 /**
  *    Everything below this is starter code.		  
  */
@@ -93,7 +160,4 @@ public class A3Driver
 			 * 
 
 		}
-*/		
-	  }
-
-}
+*/	
